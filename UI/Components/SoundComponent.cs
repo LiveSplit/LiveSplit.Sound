74e1ce559ddc0275ed6342ba2d33dc4d8bdbb916
CryZe <cryze92@gmail.com>
2014-07-24 17:29:17 +0200

    Implemented the core logic

--- UI/Components/SoundComponent.cs
+++ UI/Components/SoundComponent.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,6 +17,9 @@ namespace LiveSplit.UI.Components
     public class SoundComponent : LogicComponent
     {
         public LiveSplitState State { get; set; }
+        public MediaPlayer.IMediaPlayer Player { get; set; }
+        public SoundSettings Settings { get; set; }
+
 
         public override string ComponentName
         {
@@ -24,7 +28,62 @@ namespace LiveSplit.UI.Components
 
         public SoundComponent(LiveSplitState state)
         {
+            Settings = new SoundSettings();
             State = state;
+            Player = new MediaPlayer.MediaPlayer();
+
+            State.OnStart += State_OnStart;
+            State.OnSplit += State_OnSplit;
+            State.OnSkipSplit += State_OnSkipSplit;
+            State.OnUndoSplit += State_OnUndoSplit;
+            State.OnPause += State_OnPause;
+            State.OnResume += State_OnResume;
+            State.OnReset += State_OnReset;
+
+            Settings.Split = @"D:\Musik\Speedrun Sounds\Time.mp3";
+            Settings.Reset = @"D:\Musik\Speedrun Sounds\Well_shit.mp3";
+        }
+
+        void State_OnReset(object sender, EventArgs e)
+        {
+            PlaySound(Settings.Reset);
+        }
+
+        void State_OnResume(object sender, EventArgs e)
+        {
+            PlaySound(Settings.Resume);
+        }
+
+        void State_OnPause(object sender, EventArgs e)
+        {
+            PlaySound(Settings.Pause);
+        }
+
+        void State_OnUndoSplit(object sender, EventArgs e)
+        {
+            PlaySound(Settings.UndoSplit);
+        }
+
+        void State_OnSkipSplit(object sender, EventArgs e)
+        {
+            PlaySound(Settings.SkipSplit);
+        }
+
+        void State_OnSplit(object sender, EventArgs e)
+        {
+            if (State.CurrentPhase == TimerPhase.Ended)
+            {
+                PlaySound(Settings.PersonalBest);
+            }
+            else
+            {
+                PlaySound(Settings.Split);
+            }
+        }
+
+        void State_OnStart(object sender, EventArgs e)
+        {
+            PlaySound(Settings.StartTimer);
         }
 
         public override Control GetSettingsControl(LayoutMode mode)
@@ -47,6 +106,17 @@ namespace LiveSplit.UI.Components
 
         public override void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
         {
+        }
+
+        public void PlaySound(String location)
+        {
+            if (!String.IsNullOrEmpty(location))
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Player.Open(location);
+                });
+            }
         }
     }
 }
--- UI/Components/SoundSettings.cs
+++ UI/Components/SoundSettings.cs
@@ -17,118 +17,26 @@ namespace LiveSplit.UI.Components
 {
     public partial class SoundSettings : UserControl
     {
-        public Color TextColor { get; set; }
-        public bool OverrideTextColor { get; set; }
-        public TimeAccuracy Accuracy { get; set; }
-        public bool DropDecimals { get; set; }
-
-        public Color BackgroundColor { get; set; }
-        public Color BackgroundColor2 { get; set; }
-        public GradientType BackgroundGradient { get; set; }
-        public String GradientString
-        {
-            get { return BackgroundGradient.ToString(); }
-            set { BackgroundGradient = (GradientType)Enum.Parse(typeof(GradientType), value); }
-        }
-
-        public String Comparison { get; set; }
-        public LiveSplitState CurrentState { get; set; }
-        public bool Display2Rows { get; set; }
-
-        public LayoutMode Mode { get; set; }
+        public String GoldSplit { get; set; }
+        public String Split { get; set; }
+        public String UndoSplit { get; set; }
+        public String SkipSplit { get; set; }
+        public String PersonalBest { get; set; }
+        public String NotAPersonalBest { get; set; }
+        public String Reset { get; set; }
+        public String Pause { get; set; }
+        public String Resume { get; set; }
+        public String StartTimer { get; set; }
 
         public SoundSettings()
         {
             InitializeComponent();
-
-            TextColor = Color.FromArgb(255, 255, 255);
-            OverrideTextColor = false;
-            Accuracy = TimeAccuracy.Seconds;
-            BackgroundColor = Color.Transparent;
-            BackgroundColor2 = Color.Transparent;
-            BackgroundGradient = GradientType.Plain;
-            Comparison = "Current Comparison";
-            Display2Rows = false;
-
-            chkOverrideTextColor.DataBindings.Add("Checked", this, "OverrideTextColor", false, DataSourceUpdateMode.OnPropertyChanged);
-            btnTextColor.DataBindings.Add("BackColor", this, "TextColor", false, DataSourceUpdateMode.OnPropertyChanged);
-
-            cmbGradientType.SelectedIndexChanged += cmbGradientType_SelectedIndexChanged;
-            cmbGradientType.DataBindings.Add("SelectedItem", this, "GradientString", false, DataSourceUpdateMode.OnPropertyChanged);
-            btnColor1.DataBindings.Add("BackColor", this, "BackgroundColor", false, DataSourceUpdateMode.OnPropertyChanged);
-            btnColor2.DataBindings.Add("BackColor", this, "BackgroundColor2", false, DataSourceUpdateMode.OnPropertyChanged);
-            cmbComparison.SelectedIndexChanged += cmbComparison_SelectedIndexChanged;
-            cmbComparison.DataBindings.Add("SelectedItem", this, "Comparison", false, DataSourceUpdateMode.OnPropertyChanged);
-            chkDropDecimals.DataBindings.Add("Checked", this, "DropDecimals", false, DataSourceUpdateMode.OnPropertyChanged);
-            this.Load += DeltaSettings_Load;
-
-            rdoSeconds.CheckedChanged += rdoSeconds_CheckedChanged;
-            rdoHundredths.CheckedChanged += rdoHundredths_CheckedChanged;
-
-            chkOverrideTextColor.CheckedChanged += chkOverrideTextColor_CheckedChanged;
-        }
-
-        void chkOverrideTextColor_CheckedChanged(object sender, EventArgs e)
-        {
-            label1.Enabled = btnTextColor.Enabled = chkOverrideTextColor.Checked;
-        }
-        void cmbComparison_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            Comparison = cmbComparison.SelectedItem.ToString();
         }
 
         void DeltaSettings_Load(object sender, EventArgs e)
         {
-            chkOverrideTextColor_CheckedChanged(null, null);
-            cmbComparison.Items.Clear();
-            cmbComparison.Items.Add("Current Comparison");
-            cmbComparison.Items.AddRange(CurrentState.Run.Comparisons.Where(x => x != BestSplitTimesComparisonGenerator.ComparisonName && x != NoneComparisonGenerator.ComparisonName).ToArray());
-            if (!cmbComparison.Items.Contains(Comparison))
-                cmbComparison.Items.Add(Comparison);
-            rdoSeconds.Checked = Accuracy == TimeAccuracy.Seconds;
-            rdoTenths.Checked = Accuracy == TimeAccuracy.Tenths;
-            rdoHundredths.Checked = Accuracy == TimeAccuracy.Hundredths;
-            if (Mode == LayoutMode.Horizontal)
-            {
-                chkTwoRows.Enabled = false;
-                chkTwoRows.DataBindings.Clear();
-                chkTwoRows.Checked = true;
-            }
-            else
-            {
-                chkTwoRows.Enabled = true;
-                chkTwoRows.DataBindings.Clear();
-                chkTwoRows.DataBindings.Add("Checked", this, "Display2Rows", false, DataSourceUpdateMode.OnPropertyChanged);
-            }
         }
 
-        void cmbGradientType_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            btnColor1.Visible = cmbGradientType.SelectedItem.ToString() != "Plain";
-            btnColor2.DataBindings.Clear();
-            btnColor2.DataBindings.Add("BackColor", this, btnColor1.Visible ? "BackgroundColor2" : "BackgroundColor", false, DataSourceUpdateMode.OnPropertyChanged);
-            GradientString = cmbGradientType.SelectedItem.ToString();
-        }
-
-        void rdoHundredths_CheckedChanged(object sender, EventArgs e)
-        {
-            UpdateAccuracy();
-        }
-
-        void rdoSeconds_CheckedChanged(object sender, EventArgs e)
-        {
-            UpdateAccuracy();
-        }
-
-        void UpdateAccuracy()
-        {
-            if (rdoSeconds.Checked)
-                Accuracy = TimeAccuracy.Seconds;
-            else if (rdoTenths.Checked)
-                Accuracy = TimeAccuracy.Tenths;
-            else
-                Accuracy = TimeAccuracy.Hundredths;
-        }
 
         private T ParseEnum<T>(XmlElement element)
         {
@@ -143,30 +51,12 @@ namespace LiveSplit.UI.Components
                 version = Version.Parse(element["Version"].InnerText);
             else
                 version = new Version(1, 0, 0, 0);
-            TextColor = ParseColor(element["TextColor"]);
-            OverrideTextColor = Boolean.Parse(element["OverrideTextColor"].InnerText);
-            Accuracy = ParseEnum<TimeAccuracy>(element["Accuracy"]);
-            BackgroundColor = ParseColor(element["BackgroundColor"]);
-            BackgroundColor2 = ParseColor(element["BackgroundColor2"]);
-            GradientString = element["BackgroundGradient"].InnerText;
-            Comparison = element["Comparison"].InnerText;
-            Display2Rows = Boolean.Parse(element["Display2Rows"].InnerText);
-            DropDecimals = Boolean.Parse(element["DropDecimals"].InnerText);
         }
 
         public XmlNode GetSettings(XmlDocument document)
         {
             var parent = document.CreateElement("Settings");
             parent.AppendChild(ToElement(document, "Version", "1.4"));
-            parent.AppendChild(ToElement(document, TextColor, "TextColor"));
-            parent.AppendChild(ToElement(document, "OverrideTextColor", OverrideTextColor));
-            parent.AppendChild(ToElement(document, "Accuracy", Accuracy));
-            parent.AppendChild(ToElement(document, BackgroundColor, "BackgroundColor"));
-            parent.AppendChild(ToElement(document, BackgroundColor2, "BackgroundColor2"));
-            parent.AppendChild(ToElement(document, "BackgroundGradient", BackgroundGradient));
-            parent.AppendChild(ToElement(document, "Comparison", Comparison));
-            parent.AppendChild(ToElement(document, "Display2Rows", Display2Rows));
-            parent.AppendChild(ToElement(document, "DropDecimals", DropDecimals));
             return parent;
         }
 
