7ccbe26c5c7949d4374061ea0733610bb96ae9dc
CryZe <cryze92@gmail.com>
2014-07-24 14:45:46 +0200

    Initial Commit

---
+++ UI/Components/SoundComponent.cs
@@ -0,0 +1,52 @@
+﻿using LiveSplit.Model;
+using LiveSplit.Model.Comparisons;
+using LiveSplit.TimeFormatters;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiveSplit.UI.Components
+{
+    public class SoundComponent : LogicComponent
+    {
+        public LiveSplitState State { get; set; }
+
+        public override string ComponentName
+        {
+            get { return "Sound Effects"; }
+        }
+
+        public SoundComponent(LiveSplitState state)
+        {
+            State = state;
+        }
+
+        public override Control GetSettingsControl(LayoutMode mode)
+        {
+            return null;
+        }
+
+        public override System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+        {
+            return document.CreateElement("x");
+        }
+
+        public override void SetSettings(System.Xml.XmlNode settings)
+        {
+        }
+
+        public override void RenameComparison(string oldName, string newName)
+        {
+        }
+
+        public override void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+        }
+    }
+}
---
+++ UI/Components/SoundFactory.cs
@@ -0,0 +1,64 @@
+﻿using LiveSplit.Delta;
+using LiveSplit.Model;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: ComponentFactory(typeof(SoundFactory))]
+
+namespace LiveSplit.Delta
+{
+    public class SoundFactory : IComponentFactory
+    {
+        public string ComponentName
+        {
+            get { return "Sound Effects"; }
+        }
+
+        public string Description
+        {
+            get { return "Plays sound effects for different situations."; }
+        }
+
+        public ComponentCategory Category
+        {
+            get { return ComponentCategory.Media; }
+        }
+
+        public IComponent Create(LiveSplitState state)
+        {
+            return new SoundComponent(state);
+        }
+
+        public string UpdateName
+        {
+            get { return ComponentName; }
+        }
+
+        public string XMLURL
+        {
+#if RELEASE_CANDIDATE
+            get { return "http://livesplit.org/update_rc_sdhjdop/Components/update.LiveSplit.Sound.xml"; }
+#else
+            get { return "http://livesplit.org/update/Components/update.LiveSplit.Sound.xml"; }
+#endif
+        }
+
+        public string UpdateURL
+        {
+#if RELEASE_CANDIDATE
+            get { return "http://livesplit.org/update_rc_sdhjdop/"; }
+#else
+            get { return "http://livesplit.org/update/"; }
+#endif
+        }
+
+        public Version Version
+        {
+            get { return Version.Parse("1.0.0"); }
+        }
+    }
+}
