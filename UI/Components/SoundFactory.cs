ffe4a108c9c9825afeba8b1e17e5d4af41f3cede
wooferzfg1 <spapushin@gmail.com>
2015-01-30 17:17:15 -0600

    changed version to 1.1

--- UI/Components/SoundFactory.cs
+++ UI/Components/SoundFactory.cs
@@ -57,7 +57,7 @@ namespace LiveSplit.UI.Components
 
         public Version Version
         {
-            get { return Version.Parse("1.0.0"); }
+            get { return Version.Parse("1.1.0"); }
         }
     }
 }
