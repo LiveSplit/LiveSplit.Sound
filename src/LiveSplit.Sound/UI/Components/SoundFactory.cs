1c17cd2931fa2093c6ca00238bd57df9d4eb5ba8
Ero <56401411+just-ero@users.noreply.github.com>
2024-09-05 03:42:36 +0200

    Add `.editorconfig` & apply style rules (#15)
    * Add `.editorconfig`
    
    * Upgrade to C# 12
    
    * Use file-scoped namespaces, move `using` directives, fix whitespace
    
    * `IDE0005`, `IDE0065`, `IDE0161`
    
    * Apply member body preferences
    
    * `IDE0021`, `IDE0022`, `IDE0023`, `IDE0024`, `IDE0025`, `IDE0026`, `IDE0027`, `IDE0053`, `IDE0061`
    
    * Apply braces, parentheses, new-line preferences
    
    * `IDE0011`, `IDE0047`, `IDE0048`, `IDE2000`, `IDE2001`, `IDE2002`, `IDE2003`, `IDE2004`, `IDE2005`, `IDE2006`
    
    * Use object initializer, collection initializer/expression
    
    * `IDE0017`, `IDE0028`, `IDE0300`, `IDE0301`, `IDE0302`, `IDE0303`, `IDE0304`, `IDE0305`
    
    * Apply `var` preferences, simplify `new` expression
    
    * `IDE0007`, `IDE0008`, `IDE0090`
    
    * Use index/range operator
    
    * `IDE0056`, `IDE0057`
    
    * Enforce code style in build

--- src/LiveSplit.Sound/UI/Components/SoundFactory.cs
+++ src/LiveSplit.Sound/UI/Components/SoundFactory.cs
@@ -1,27 +1,30 @@
-﻿using LiveSplit.Model;
+﻿using System;
+
+using LiveSplit.Model;
 using LiveSplit.UI.Components;
-using System;
 
 [assembly: ComponentFactory(typeof(SoundFactory))]
 
-namespace LiveSplit.UI.Components
+namespace LiveSplit.UI.Components;
+
+public class SoundFactory : IComponentFactory
 {
-    public class SoundFactory : IComponentFactory
+    public string ComponentName => "Sound Effects";
+
+    public string Description => "Plays sound effects for different situations.";
+
+    public ComponentCategory Category => ComponentCategory.Media;
+
+    public IComponent Create(LiveSplitState state)
     {
-        public string ComponentName => "Sound Effects";
-
-        public string Description => "Plays sound effects for different situations.";
-
-        public ComponentCategory Category => ComponentCategory.Media;
-
-        public IComponent Create(LiveSplitState state) => new SoundComponent(state);
-
-        public string UpdateName => ComponentName;
-
-        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Sound.xml";
-
-        public string UpdateURL => "http://livesplit.org/update/";
-
-        public Version Version => Version.Parse("1.8.29");
+        return new SoundComponent(state);
     }
+
+    public string UpdateName => ComponentName;
+
+    public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Sound.xml";
+
+    public string UpdateURL => "http://livesplit.org/update/";
+
+    public Version Version => Version.Parse("1.8.29");
 }
